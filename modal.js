2bf6a834e6df43a24c331b90a04f7866ed50f4ee
delimaqz <raphaelldelimaa@gmail.com>
2022-10-25 13:22:17 -0300

    mundança para trocar background

--- modal.js
+++ modal.js
@@ -1,4 +1,22 @@
+const element = document.querySelector("body")
+element.addEventListener('click', changeBackground)
+
 function changeBackground(){
-    const element = document.querySelector("body")
-    element.classList.toggle('active')
+   element.classList.toggle('active')
 }
+
+let image = document.querySelector("#image")
+image.addEventListener('click', changeImage)
+let changedImg = true
+
+function changeImage(){
+   image.classList.toggle('active')
+   if(changedImg){
+        image.src = './assets/SwitcherLight.svg'
+        changedImg = false
+   }else{
+        image.src = './assets/Switcher.svg'
+        changedImg = true
+    }
+}
+
